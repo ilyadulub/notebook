1c626f4e083ca3bf8fbd36eddac8109ab48daeb2
Ilya Dulub <ilyadulub@yandex.ru>
2023-11-02 18:29:05 +0300

    add migration and model for notes

---
+++ database/migrations/2023_11_02_142424_create_notes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notes', function (Blueprint $table) {
+            $table->id();
+            $table->string('full_name', 150);
+            $table->string('company', 50)->nullable();
+            $table->string('phone', 12)->unique();
+            $table->string('email', 50)->unique();
+            $table->date('birthday')->nullable();
+            $table->string('photo')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notes');
+    }
+}
